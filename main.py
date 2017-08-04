121ad0d10c53ae41ac386e66f9e25d55f73490d6
Nicco Nocon <nocon_1_3@yahoo.com>
2017-08-05 00:01:32 +0800

    Added main_test.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+import logging
+
+from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return render_template("index.html")
-	
-if __name__ == "__main__":
-	app.run()
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
