7df82ffb36bfe646f58eb64573a41df4ce1d1f1c
Nicco Nocon <nocon_1_3@yahoo.com>
2017-08-15 22:06:03 +0800

    POST Test

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Translate(webapp2.RequestHandler):
         template_vars = {
           'translation': translation
         }
-        template = JINJA_ENVIRONMENT.get_template('index.html/#translate')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.out.write(template.render(template_vars))
 
 
