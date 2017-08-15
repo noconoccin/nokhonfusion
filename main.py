7ff82ced3e06b3d3087cd2aecd756d67bfdc08d6
Nicco Nocon <nocon_1_3@yahoo.com>
2017-08-15 19:42:00 +0800

    template_vars indention fixed

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
 	    translation = "[Test] This is the translation."
-		template_vars = {
-		  'translation': translation
+	    template_vars = {
+	      'translation': translation
 		}
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.out.write(template.render(template_vars))
