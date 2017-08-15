99ad697f7215bcd7cfd7118f19ec2502607144ad
Nicco Nocon <nocon_1_3@yahoo.com>
2017-08-15 18:21:27 +0800

    Updates on Indentions and Website MT Labels

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.out.write(template.render())
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.out.write(template.render())
 
 
 app = webapp2.WSGIApplication([
