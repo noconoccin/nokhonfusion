9aa7388ffe64d41942417d206368cae55670dbcb
Nicco Nocon <nocon_1_3@yahoo.com>
2017-08-15 22:15:01 +0800

    Input Test

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         fctext = self.request.get('fctext')
+        print(fctext)
         translation = "[Test] This is the translation."
         template_vars = {
           'translation': translation
